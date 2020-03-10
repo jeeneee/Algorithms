73c23de62fdf23c542e4157cee3dda7b25cba603
jeenee94 <woojin7124@naver.com>
2020-03-10 14:54:16 +0900

    삼성 코딩테스트 기출문제

---
+++ Algorithms-CPP/백준/시뮬레이션(Simulation)/17140_이차원 배열과 연산.cpp
@@ -0,0 +1,9 @@
+//
+//  17140_이차원 배열과 연산.cpp
+//  Algorithms-CPP
+//
+//  Created by 장우진 on 2020/03/10.
+//  Copyright © 2020 장우진. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Algorithms-CPP/백준/시뮬레이션(Simulation)/17141_연구소 2.cpp
@@ -0,0 +1,9 @@
+//
+//  17141_연구소 2.cpp
+//  Algorithms-CPP
+//
+//  Created by 장우진 on 2020/03/10.
+//  Copyright © 2020 장우진. All rights reserved.
+//
+
+#include <stdio.h>
