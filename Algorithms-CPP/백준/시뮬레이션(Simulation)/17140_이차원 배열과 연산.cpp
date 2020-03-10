c855e0f93dcb35660f421591846cd3359919a73e
jeenee94 <woojin7124@naver.com>
2020-03-10 14:56:18 +0900

    삼성 코딩테스트 기출문제

--- Algorithms-CPP/백준/시뮬레이션(Simulation)/17140_이차원 배열과 연산.cpp
+++ Algorithms-CPP/백준/시뮬레이션(Simulation)/17140_이차원 배열과 연산.cpp
@@ -5,5 +5,100 @@
 //  Created by 장우진 on 2020/03/10.
 //  Copyright © 2020 장우진. All rights reserved.
 //
+/*
+ 문제: 한 행 또는 열에 있는 수를 정렬하려면, 각각의 수가 몇 번 나왔는지 알아야 한다.
+	  그 다음, 수의 등장 횟수가 커지는 순으로, 그러한 것이 여러가지면 수가 커지는 순으로 정렬한다.
+	  그 다음에는 배열 A에 정렬된 결과를 다시 넣어야 한다.
+	  정렬된 결과를 배열에 넣을 때는, 수와 등장 횟수를 모두 넣으며, 순서는 수가 먼저이다.
+	  예를 들어, [3, 1, 1]에는 3이 1번, 1가 2번 등장한다.
+	  따라서, 정렬된 결과는 [3, 1, 1, 2]가 된다. 다시 이 배열에는 3이 1번, 1이 2번, 2가 1번 등장한다.
+	  다시 정렬하면 [2, 1, 3, 1, 1, 2]가 된다.
+ 해설: R연산과 C연산이 나누어져있지만 한 연산만 구현하고 다른 하나는 A행렬을 전치시켜 구한다.
+	  정렬할 때에는 등장 횟수의 오름차 순(우선순위1), 해당 수의 오름차 순(우선순위2)으로 정렬한다.
+	  (그냥 pair<int, int>로 {등장 횟수, 넘버}를 넣어주고 그대로 정렬시키면 된다.
+ 알고리즘: 정렬
+ O(N^2 * lgN)
+ */
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
 
-#include <stdio.h>
+const int MAX = 100 + 1;
+int A[MAX][MAX], cnt[MAX]; // A=2차원 배열, cnt=등장 횟수
+vector<pair<int, int>> v[MAX]; // {등장 횟수, 넘버}
+int R, C;
+
+void Cal() {
+	for (int i = 1; i <= R; ++i) {
+		int maxNum = 0; // 끝까지 확인할 필요 없도록 최대 수를 저장한다
+		for (int j = 1; j <= C; ++j) {
+			if (A[i][j] == 0) continue;	// 0 무시
+			cnt[A[i][j]]++;
+			maxNum = max(maxNum, A[i][j]);
+			A[i][j] = 0; // 0 초기화
+		}
+		for (int num = 1; num <= maxNum; ++num)
+			if (cnt[num]) {
+				v[i].push_back( {cnt[num], num} );
+				cnt[num] = 0; // 0 초기화
+			}
+	}
+	C = 0;
+	for (int i = 1; i <= R; ++i) {
+		sort(v[i].begin(), v[i].end()); // 정렬
+		int j = 0; // 열(1부터)
+		for (auto& a : v[i]) {
+			A[i][++j] = a.second;
+			A[i][++j] = a.first;
+		}
+		v[i].clear(); // 벡터 초기화
+		C = max(C, j); // 최대 열 계산
+	}
+}
+
+// 전치 행렬
+void transpose() {
+	int temp[MAX][MAX];
+	memset(temp, 0, sizeof(temp));
+	for (int i = 1; i <= C; ++i)
+		for (int j = 1; j <= R; ++j)
+			temp[i][j] = A[j][i];
+	memcpy(A, temp, sizeof(temp));
+}
+
+int main() {
+	int r, c, k;
+	cin >> r >> c >> k;
+	for (int i = 1; i <= 3; ++i) {
+		for (int j = 1; j <= 3; ++j) {
+			cin >> A[i][j];
+		}
+	}
+	
+	R = 3; C = 3;
+	int time = 0;
+	while (1) {
+		// 기저 사례
+		if (A[r][c] == k) {
+			cout << time << '\n';
+			return 0;
+		} else if (time > 100) {
+			cout << -1 << '\n';
+			return 0;
+		}
+		// 연산
+		if (R < C) {
+			transpose();
+			swap(R, C);
+			Cal();
+			transpose();
+			swap(R, C);
+		}
+		else
+			Cal();
+		time++;
+	}
+	return 0;
+}
--- Algorithms-CPP/백준/시뮬레이션(Simulation)/17141_연구소 2.cpp
+++ Algorithms-CPP/백준/시뮬레이션(Simulation)/17141_연구소 2.cpp
@@ -5,5 +5,107 @@
 //  Created by 장우진 on 2020/03/10.
 //  Copyright © 2020 장우진. All rights reserved.
 //
+/*
+ 문제: 바이러스가 퍼지는 평범한 조합 문제(BFS를 곁들인).
+	  완전 탐색으로 중간중간마다 빈 칸이 있는지 확인해야 한다.
+ 알고리즘: 백트래킹 + BFS
+ */
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+using namespace std;
 
-#include <stdio.h>
+const int dx[] = {0, 0, -1, 1};
+const int dy[] = {-1, 1, 0, 0};
+const int MAX = 50;
+const int INF = 987654321;
+int map[MAX][MAX], c_map[MAX][MAX];
+vector<pair<int, int>> virus;
+int N, M;
+int ans = INF;
+
+void dfs(int x, int cnt) {
+	if (cnt == M) {
+		queue<pair<int, int>> q;
+		bool alreadyCompleted = true;
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < N; ++j) {
+				if (map[i][j] == 2)
+					q.push( {i, j} );
+				else if (map[i][j] == 0)
+					alreadyCompleted = false;
+			}
+		// 이미 빈 칸이 없는 경우
+		if (alreadyCompleted) {
+			ans = 0;
+			return;
+		}
+		int step = 0;
+		bool hasZero = false;
+		while (!q.empty()) {
+			step++;
+			int size = q.size();
+			hasZero = false;
+			// 단계별로 바이러스 확산
+			while (size--) {
+				int x = q.front().first;
+				int y = q.front().second;
+				q.pop();
+				for (int i = 0; i < 4; ++i) {
+					int nx = x + dx[i];
+					int ny = y + dy[i];
+					if ((0 <= nx && nx < N) && (0 <= ny && ny < N)) {
+						if (map[nx][ny] == 0) {
+							map[nx][ny] = 2;
+							q.push( {nx, ny} );
+						}
+					}
+				}
+			}
+			// 빈 칸 확인
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					if (map[i][j] == 0) {
+						hasZero = true;
+						break;
+					}
+				}
+				if (hasZero) break;
+			}
+			// 빈 칸이 없는 경우 종료
+			if (!hasZero)
+				break;
+		}
+		if (hasZero) step = INF; // 여전히 빈 칸이 있는 경우
+		ans = min(ans, step);
+		return;
+	}
+	// 백트래킹
+	for (int i = x; i < virus.size(); ++i) {
+		memcpy(c_map, map, sizeof(map));
+		map[virus[i].first][virus[i].second] = 2;
+		dfs(i+1, cnt+1);
+		memcpy(map, c_map, sizeof(map));
+		map[virus[i].first][virus[i].second] = 0;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> map[i][j];
+			if (map[i][j] == 2) {
+				virus.push_back( {i, j} );
+				map[i][j] = 0;
+			}
+		}
+	}
+	dfs(0, 0);
+	if (ans == INF) ans = -1;
+	cout << ans << '\n';
+	return 0;
+}
