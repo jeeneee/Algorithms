573f4ea9489da19c45b9021ea5abb3a2645706b6
jeenee94 <woojin7124@naver.com>
2020-09-01 23:31:39 +0900

    2020/09/01

--- Algorithms-CPP/백준/벨만-포드(Bellman-Ford)/11657_타임머신.cpp
+++ Algorithms-CPP/백준/벨만-포드(Bellman-Ford)/11657_타임머신.cpp
@@ -11,53 +11,39 @@
 #include <algorithm>
 using namespace std;
 
-typedef pair<int, int> pii;
+struct Node { int next, cost; };
+const int INF = 1e9;
 
-const int INF = 987654321;
-
-vector<vector<pii> > graph;
-vector<int> dist;
-int N, M;
-
-void bellman(int s) {
-    dist[s] = 0;
-    bool negCycle = false;
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            for (auto &v : graph[j]) {
-                if (dist[j] != INF && dist[j] + v.second < dist[v.first]) {
-                    dist[v.first] = dist[j] + v.second;
-                    if (i == N)
-                        negCycle = true;
-                }
-            }
-        }
-    }
-    
-    if (negCycle)
-        cout << -1 << '\n';
-    else
-        for (int i = 2; i <= N; ++i) {
-            if (dist[i] == INF) dist[i] = -1;
-            cout << dist[i] << '\n';
-        }
-}
+vector<vector<Node>> adj;
+vector<long long> dist;
+int n, m, a, b, c;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N >> M;
-    graph.resize(N + 1);
-    dist.resize(N + 1);
-    
-    int u, v, w;
-    for (int i = 0; i < M; ++i) {
-        cin >> u >> v >> w;
-        graph[u].push_back(make_pair(v, w));
-    }
-    
-    fill(dist.begin(), dist.end(), INF);
-    
-    bellman(1);
-    
-    return 0;
+   scanf("%d %d", &n, &m);
+   adj.resize(n + 1);
+   dist.resize(n + 1, INF);
+   while (m--) {
+      scanf("%d %d %d", &a, &b, &c);
+      adj[a].push_back({ b, c });
+   }
+
+   dist[1] = 0;
+   bool flag = false;
+   
+   for (int i = 1; i <= n && !flag; ++i)
+      for (int j = 1; j <= n && !flag; ++j)
+         for (int k = 0; k < adj[j].size() && !flag; ++k) {
+            int next = adj[j][k].next;
+            int cost = adj[j][k].cost;
+            if (dist[j] != INF && dist[next] > dist[j] + cost) {
+               dist[next] = dist[j] + cost;
+               if (i == n) flag = true;
+            }
+         }
+
+   if (flag) puts("-1");
+   else for (int i = 2; i <= n; ++i)
+        printf("%lld\n", dist[i] == INF ? -1 : dist[i]);
+
+   return 0;
 }
--- Algorithms-CPP/백준/벨만-포드(Bellman-Ford)/1865_웜홀.cpp
+++ Algorithms-CPP/백준/벨만-포드(Bellman-Ford)/1865_웜홀.cpp
@@ -11,62 +11,45 @@
 #include <algorithm>
 using namespace std;
 
-typedef pair<int, int> pii;
-
-const int INF = 987654321;
-
-vector<vector<pii> > graph;
-vector<int> cost;
-int N, M, W;
-
-void bellman(int s) {
-    cost[s] = 0;
-    bool negCycle = false;
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j <= N; ++j) {
-            for (auto &v : graph[j]) {
-                if (cost[j] != INF && cost[j] + v.second < cost[v.first]) {
-                    cost[v.first] = cost[j] + v.second;
-                    if (i == N)
-                        negCycle = true;
-                }
-            }
-        }
-    }
-    
-    if (negCycle)
-        cout << "YES" << '\n';
-    else
-        cout << "NO" << '\n';
-}
+struct Node { int next, cost; };
+const int INF = 1e9;
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        graph.clear();
-        cost.clear();
-        
-        cin >> N >> M >> W;
-        graph.resize(N + 1);
-        cost.resize(N + 1);
-        
-        int u, v, w;
-        for (int i = 0; i < M; ++i) {
-            cin >> u >> v >> w;
-            graph[u].push_back(make_pair(v, w));
-            graph[v].push_back(make_pair(u, w));
-        }
-        for (int i = 0; i < W; ++i) {
-            cin >> u >> v >> w;
-            graph[u].push_back(make_pair(v, -w));
-        }
-        
-        fill(cost.begin(), cost.end(), INF);
-        
-        bellman(1);
-    }
-    
-    return 0;
+   ios::sync_with_stdio(false); cin.tie(0);
+   int tc;
+   cin >> tc;
+   while (tc--) {
+      int n, m, w, s, e, t;
+      cin >> n >> m >> w;
+      vector<vector<Node>> adj(n + 1);
+      vector<int> dist(n + 1, INF);
+
+      while (m--) {
+         cin >> s >> e >> t;
+         adj[s].push_back({ e, t });
+         adj[e].push_back({ s, t });
+      }
+      while (w--) {
+         cin >> s >> e >> t;
+         adj[s].push_back({ e, -t });
+      }
+
+      bool flag = false;
+      dist[1] = 0;
+
+      for (int i = 1; i <= n && !flag; ++i)
+         for (int j = 1; j <= n && !flag; ++j)
+            for (int k = 0; k < adj[j].size() && !flag; ++k) {
+               int next = adj[j][k].next;
+               int cost = adj[j][k].cost;
+               if (dist[next] > dist[j] + cost) {
+                  dist[next] = dist[j] + cost;
+                  if (i == n) flag = true;
+               }
+            }
+
+      if (flag) cout << "YES" << '\n';
+      else cout << "NO" << '\n';
+   }
+   return 0;
 }
