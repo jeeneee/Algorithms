5fa6c90656ca48ba24a1886bae0c51c2ba09b0cd
jeenee94 <woojin7124@naver.com>
2020-09-20 20:56:31 +0900

    2020/09/20

--- Algorithms-CPP/프로그래머스/[2019 카카오 개발자 겨울 인턴십] 튜플.cpp
+++ Algorithms-CPP/프로그래머스/[2019 카카오 개발자 겨울 인턴십] 튜플.cpp
@@ -15,42 +15,44 @@
 using namespace std;
 
 vector<int> solution(string s) {
-	vector<int> answer;
-	vector<int> bracket;
-	vector<pair<int, vector<int>>> v;
-	int num = 0;
-	bool flag = false;
-	for (int i = 1; i < s.size()-1; ++i) {
-		if (s[i] == '{')
-			flag = true;
-		else if ('0' <= s[i] && s[i] <= '9')
-			num = num * 10 + s[i] - '0';
-		else if (s[i] == ',' && flag) {
-			bracket.push_back(num);
-			num = 0;
-		}
-		else if (s[i] == '}') {
-			bracket.push_back(num);
-			num = 0;
-			v.push_back( {bracket.size(), bracket} );
-			bracket.clear();
-			flag = false;
+    vector<int> answer;
+	set<int> ss;
+	vector<int> v[501];
+	int i = 0, j = 0;
+	int total = 0;
+	for (i = 1; i < s.size() - 1; ++i) {
+		if (s[i] == '{') {
+			int cnt = 0;
+			int temp = 0;
+			vector<int> temp_v;
+			for (j = i+1; s[j] != '}'; ++j) {
+				if (s[j] == ',') {
+					cnt++;
+					temp_v.push_back(temp);
+					temp = 0;
+				}
+				else temp = temp * 10 + s[j] - '0';
+			}
+			cnt++;
+			temp_v.push_back(temp);
+			v[cnt] = temp_v;
+			total = max(total, cnt);
+			i = j + 1;
 		}
 	}
-	sort(v.begin(), v.end());
-	set<int> chk;
-	for (int i = 0; i < v.size(); ++i)
-		for (int j = 0; j < v[i].second.size(); ++j)
-			if (!chk.count(v[i].second[j])) {
-				chk.insert(v[i].second[j]);
-				answer.push_back(v[i].second[j]);
-			}
-	return answer;
+	for (int i = 1; i <= total; ++i) {
+		for (int j = 0; j < i; ++j) {
+			if (ss.count(v[i][j])) continue;
+			answer.push_back(v[i][j]);
+			ss.insert(v[i][j]);
+		}
+	}
+    return answer;
 }
 
 int main() {
-	string s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
-	for (auto& a : solution(s))
+	string s = "{{4,2,3},{3},{2,3,4,1},{2,3}}";
+	for (auto a : solution(s))
 		cout << a << ' ';
 	cout << endl;
 	return 0;
